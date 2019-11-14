735f1107f2c987005b25146c1d9e8c425d2c508e
Ajiaxi <zzl1230bk@163.com>
2019-11-14 23:12:13 +0800

    last time

--- working_log/settings.py
+++ working_log/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(7=62%d))8$d*81w^y9m+9+xpux5oi6-f8e5jeh@9d-l-o8m*m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
